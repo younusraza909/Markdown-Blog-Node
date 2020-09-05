7eb5a360e3759199fea5e905cd5a975bee103ae8
younusraza909 <younusraza909@gmail.com>
2020-09-04 20:53:42 -0700

    New post template donw

--- server.js
+++ server.js
@@ -2,8 +2,19 @@ const express = require("express");
 
 const app = express();
 
+app.set("view engine", "ejs");
+
+app.use("/articles", require("./routes/articles"));
+
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  const articles = [
+    {
+      title: "Test Article",
+      createdAt: new Date(),
+      description: "Test Description",
+    },
+  ];
+  res.render("articles/index", { articles: articles });
 });
 
 app.listen(5000);
