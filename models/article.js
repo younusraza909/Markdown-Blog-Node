ea991d3e4dc8f8fdab273cfe7f4b2fec50427cc7
younusraza909 <younusraza909@gmail.com>
2020-09-05 02:46:36 -0700

    Finished With App

--- models/article.js
+++ models/article.js
@@ -1,6 +1,13 @@
 const mongoose = require("mongoose");
 const marked = require("marked");
 const slugify = require("slugify");
+//it will return fucntion
+const createDomPurify = require("dompurify");
+//we only want jsdom portion thats why use curly braces
+const { JSDOM } = require("jsdom");
+
+//i will sanitize out html
+const dompurify = createDomPurify(new JSDOM().window);
 
 const articleSchmea = new mongoose.Schema({
   title: {
@@ -23,11 +30,18 @@ const articleSchmea = new mongoose.Schema({
     required: true,
     unique: true,
   },
+  sanitizedHtml: {
+    type: String,
+    required: true,
+  },
 });
 
 articleSchmea.pre("validate", function (next) {
   if (this.title) {
     this.slug = slugify(this.title, { lower: true, strict: true });
+  }
+  if (this.markdown) {
+    this.sanitizedHtml = dompurify.sanitize(marked(this.markdown));
   }
   next();
 });
--- routes/articles.js
+++ routes/articles.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const Article = require("./../models/article");
+const { findById } = require("./../models/article");
 
 router.get("/new", (req, res) => {
   res.render("articles/new", { article: new Article() });
+});
+
+router.get("/edit/:id", async (req, res) => {
+  const article = await Article.findById(req.params.id);
+  res.render("articles/edit", { article: article });
 });
 
 router.get("/:slug", async (req, res) => {
@@ -24,6 +30,28 @@ router.post("/", async (req, res) => {
   } catch (e) {
     console.log(e);
     res.render("articles/new", { article: article });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  await Article.findByIdAndDelete(req.params.id);
+  res.redirect("/");
+});
+
+router.put("/:id", async (req, res) => {
+  let article = {
+    title: req.body.title,
+    description: req.body.description,
+    markdown: req.body.markdown,
+  };
+  try {
+    article = await Article.findByIdAndUpdate(req.params.id, article, {
+      new: true,
+    });
+    res.redirect(`/articles/${article.slug}`);
+  } catch (e) {
+    console.log(e);
+    res.render("articles/edit", { article: article });
   }
 });
 
